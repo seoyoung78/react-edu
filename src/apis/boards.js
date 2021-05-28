2ac29e550b690b6c47704dba55494403e1cafdad
seoyoung78 <tjdud0708@naver.com>
2021-05-28 14:20:41 +0900

    axios

--- src/apis/boards.js
+++ src/apis/boards.js
@@ -11,3 +11,23 @@ export function createBoardNoAttach(board) {
   // const promise = axios.post("/boards", qs.stringify(board));  //qs : btitle=제목1 & bcontent=내용1 & ...
   return promise;
 };
+
+export function createBoard(multipartFormData) {
+  return axios.post("/boards", multipartFormData); 
+}
+
+export function readBoard(bno) {
+  return axios.get("/boards/" + bno);
+}
+
+export function deleteBoard(bno) {
+  return axios.delete("/boards/" + bno);
+}
+
+export function updateBoard(board) {
+  return axios.put("/boards", board); //{"btitle":"xxx", "bcontent":"yyy", "bwriter":"zzz"}
+}
+
+export function downloadAttach(bno) {
+  return axios.get("/boards/battach/" + bno, {responseType: "blob"});
+}
